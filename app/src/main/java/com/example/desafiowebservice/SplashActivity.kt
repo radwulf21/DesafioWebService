707c7cbd7e79044b6239d735957fca12694d7c52
Raul Andrade <raul.andrade21@gmail.com>
2020-12-01 09:15:27 -0300

    Subindo SplashActivity

---
+++ app/src/main/java/com/example/desafiowebservice/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.desafiowebservice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            callLoginActivity()
+            finish()
+        }, 3000)
+    }
+
+    private fun callLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
