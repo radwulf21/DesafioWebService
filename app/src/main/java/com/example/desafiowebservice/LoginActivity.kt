c27e6708c39a767d67b41864d5be5abf2158eed4
Raul Andrade <raul.andrade21@gmail.com>
2020-12-01 10:44:50 -0300

    Subindo RegisterActivity

--- app/src/main/java/com/example/desafiowebservice/LoginActivity.kt
+++ app/src/main/java/com/example/desafiowebservice/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.example.desafiowebservice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tvCreateAccount.setOnClickListener {
+            callRegisterActivity()
+        }
+    }
+
+    fun callRegisterActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
